c85a3a4569574f61f5d7c8d05ee48a97442d4047
JC <jczas@sina.com>
2021-08-02 10:32:32 -0400

    renamed css folder and some css files

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -1,31 +1,70 @@
-import React from 'react'
-import '../cssfile/cntus.css'
-import img from '../imges/cntctus.jpg'
+import React from "react";
+import "../css/cntus.css";
+import img from "../imges/cntctus.jpg";
 const Contactus = () => {
-    return (
-       <div>
-      
-        <div class="form4 top">
+  return (
+    <div>
+      <div class="form4 top">
         <div class="container">
-       
-            <div class="row">
-                <div class="col-md-7 col-md-offset-2">
-                    <div class="form-bg">
-                        <form class="form">
-                        <div class="form-group"> <label class="sr-only">Name</label> <input type="text" class="form-control" required="" id="nameNine" placeholder="Your Name"/> </div>
-                            <div class="form-group"> <label class="sr-only">Email</label> <input type="email" class="form-control" required="" id="emaiNine" placeholder="Email Address"/> </div>
-                            <div class="form-group"> <label class="sr-only">password</label> <input type="password" class="form-control" required="" id="pword" placeholder="enter your password"/> </div>
-                            <div class="form-group"> <label class="sr-only">issue</label> <textarea class="form-control" required="" rows="7" id="messageNine" placeholder="Write your issue"></textarea> </div> <button type="submit" class="btn text-center btn-blue">Send Message</button>
-                        </form>
-                    </div>
-                </div>
+          <div class="row">
+            <div class="col-md-7 col-md-offset-2">
+              <div class="form-bg">
+                <form class="form">
+                  <div class="form-group">
+                    {" "}
+                    <label class="sr-only">Name</label>{" "}
+                    <input
+                      type="text"
+                      class="form-control"
+                      required=""
+                      id="nameNine"
+                      placeholder="Your Name"
+                    />{" "}
+                  </div>
+                  <div class="form-group">
+                    {" "}
+                    <label class="sr-only">Email</label>{" "}
+                    <input
+                      type="email"
+                      class="form-control"
+                      required=""
+                      id="emaiNine"
+                      placeholder="Email Address"
+                    />{" "}
+                  </div>
+                  <div class="form-group">
+                    {" "}
+                    <label class="sr-only">password</label>{" "}
+                    <input
+                      type="password"
+                      class="form-control"
+                      required=""
+                      id="pword"
+                      placeholder="enter your password"
+                    />{" "}
+                  </div>
+                  <div class="form-group">
+                    {" "}
+                    <label class="sr-only">issue</label>{" "}
+                    <textarea
+                      class="form-control"
+                      required=""
+                      rows="7"
+                      id="messageNine"
+                      placeholder="Write your issue"
+                    ></textarea>{" "}
+                  </div>{" "}
+                  <button type="submit" class="btn text-center btn-blue">
+                    Send Message
+                  </button>
+                </form>
+              </div>
             </div>
+          </div>
         </div>
-      
+      </div>
     </div>
-    </div>
+  );
+};
 
-    )
-}
-
-export default Contactus
+export default Contactus;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import '../cssfile/home.css'
+import React from "react";
+import "../css/home.css";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <container>
-                <p>
-                    Lorem ipsum dolor sit amet. Ea culpa veritatis vel tempora reiciendis quo mollitia tempore ex laborum officia ut fugiat dolorem. Non possimus esse aut aliquid galisum eum perferendis est provident reprehenderit quo facere sequi At voluptate autem et omnis quam. Id amet nesciunt 33 consequatur accusamus qui veritatis dolores. Non dolorum debitis eum quas perferendis ad nobis veniam.
-                    Eum reprehenderit molestiae et minus consequatur est iure deleniti in libero dolorem aut nisi quis et quaerat molestiae. Facilis voluptas eos veniam consequatur et eveniet cumque! 33 nisi vitae eos molestiae odio aut error soluta ut eligendi galisum qui necessitatibus fugit non obcaecati praesentium et voluptatem iusto.
-                    Eos necessitatibus pariatur non porro Quis At nemo voluptas. Et molestiae debitis ea obcaecati impedit rem galisum soluta?
-                </p>
-            </container>
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <container>
+        <p>
+          Lorem ipsum dolor sit amet. Ea culpa veritatis vel tempora reiciendis
+          quo mollitia tempore ex laborum officia ut fugiat dolorem. Non
+          possimus esse aut aliquid galisum eum perferendis est provident
+          reprehenderit quo facere sequi At voluptate autem et omnis quam. Id
+          amet nesciunt 33 consequatur accusamus qui veritatis dolores. Non
+          dolorum debitis eum quas perferendis ad nobis veniam. Eum
+          reprehenderit molestiae et minus consequatur est iure deleniti in
+          libero dolorem aut nisi quis et quaerat molestiae. Facilis voluptas
+          eos veniam consequatur et eveniet cumque! 33 nisi vitae eos molestiae
+          odio aut error soluta ut eligendi galisum qui necessitatibus fugit non
+          obcaecati praesentium et voluptatem iusto. Eos necessitatibus pariatur
+          non porro Quis At nemo voluptas. Et molestiae debitis ea obcaecati
+          impedit rem galisum soluta?
+        </p>
+      </container>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
