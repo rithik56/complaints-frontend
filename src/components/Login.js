966863361263072799a4661552079ad419e86a98
JC <jczas@sina.com>
2021-08-03 10:54:06 -0400

    Changed register page and login placeholder from 'Username' to 'Email'

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import "../css/login.scss"
+import React from "react";
+import "../css/login.scss";
 
 const Login = () => {
-    return (
-        <div class="wrapper">
-            <form class="login">
-                <p class="title">Log in</p>
-                <input type="text" placeholder="Username" autofocus/>
-                <i class="fa fa-user"></i>
-                <input type="password" placeholder="Password" />
-                <i class="fa fa-key"></i>
-                <a href="#">Forgot your password?</a>
-                <button>
-                    <i class="spinner"></i>
-                    <span class="state">Log in</span>
-                </button>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div class="wrapper">
+      <form class="login">
+        <p class="title">Log in</p>
+        <input type="text" placeholder="Email" autofocus />
+        <i class="fa fa-user"></i>
+        <input type="password" placeholder="Password" />
+        <i class="fa fa-key"></i>
+        <a href="#">Forgot your password?</a>
+        <button>
+          <i class="spinner"></i>
+          <span class="state">Log in</span>
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
