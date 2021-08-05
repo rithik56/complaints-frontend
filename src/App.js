e40ebc385190f281a45301f6950a807b267a3c34
JC <jczas@sina.com>
2021-08-05 09:41:15 -0400

    Added dark-theme and light-theme variable

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
@@ -8,25 +8,47 @@ import Register from "./components/Register";
 import Contactus from "./components/Contactus";
 import Onlinefiling from "./components/Onlinefiling";
 import Departments from "./components/Department";
-
-const Routing = () => {
-  return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-      <Route path="/contact-us" component={Contactus} />
-      <Route path="/online-filine" component={Onlinefiling} />
-      <Route path="/departments" component={Departments} />
-    </Switch>
-  );
-};
+import { useEffect } from "react";
+import "./css/app.css";
 
 const App = () => {
+  useEffect(() => {
+    let cacheTheme = window.localStorage.getItem("theme");
+
+    if (cacheTheme) {
+      let root = document.querySelector("#root");
+      switch (cacheTheme) {
+        case "dark-theme":
+          root.classList.add("dark-theme");
+          break;
+        case "light-theme":
+          root.classList.add("light-theme");
+          break;
+        default:
+          root.classList.add("dark-theme");
+          break;
+      }
+    } else {
+      // dark theme by default
+      window.localStorage.setItem("theme", "dark-theme");
+      let root = document.querySelector("#root");
+      root.classList.add("dark-theme");
+    }
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <Routing />
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/contact-us" component={Contactus} />
+          <Route path="/online-filine" component={Onlinefiling} />
+          <Route path="/departments" component={Departments} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 };
