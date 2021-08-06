9cdaeda148a1ae58b21e5ee673a4be2c3bfa01ad
JC <jczas@sina.com>
2021-08-06 13:06:10 -0400

    Now contact-us, register, and login forms switch theme accordingly

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,17 +3,17 @@ import "../css/login.scss";
 
 const Login = () => {
   return (
-    <div class="wrapper">
-      <form class="login">
-        <p class="title">Login</p>
-        <input type="text" placeholder="Email" autofocus />
-        <i class="fa fa-user"></i>
+    <div className="wrapper">
+      <form className="login">
+        <p className="title">Login</p>
+        <input type="text" placeholder="Email" autoFocus />
+        <i className="fa fa-user"></i>
         <input type="password" placeholder="Password" />
-        <i class="fa fa-key"></i>
+        <i className="fa fa-key"></i>
         <a href="#">Forgot your password?</a>
         <button>
-          <i class="spinner"></i>
-          <span class="state">Log In</span>
+          <i className="spinner"></i>
+          <span className="state">Log In</span>
         </button>
       </form>
     </div>
--- src/components/Register.js
+++ src/components/Register.js
@@ -24,7 +24,7 @@ const Register = () => {
   const validateInputs = () => {
     // test email
     const emailRegex =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const isEmailValid = emailRegex.test(emailRef.current.value);
     // confirm if password and confirm password matches
     const passwordMatches =
@@ -62,77 +62,76 @@ const Register = () => {
 
   return (
     <section>
-      <div class="container py-5">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col">
-            <div class="card card-registration my-4">
-              <div class="row g-0">
-                <div class="col-xl-3 d-none d-xl-block">
+      <div className="container py-5">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col">
+            <div className="card card-registration my-4">
+              <div className="row g-0">
+                <div className="col-xl-3 d-none d-xl-block">
                   <img
                     src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-registration/img4.jpg"
                     alt="People Gathered Arond"
                     className="img-fluid"
                   />
                 </div>
-                <div class="col-xl-9">
-                  <div class="card-body p-md-5 text-black">
-                    <h3 class="mb-5">
-                       Register
-                    </h3>
+                <div className="col-xl-9">
+                  <div className="card-body p-md-5 text-black">
+                    <h3 className="mb-5">Register</h3>
 
-                    <div class="row">
-                      <div class="col-md-6 mb-4">
-                        <div class="form-outline">
+                    <div className="row">
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
                           <input
                             ref={nameRef}
                             type="text"
                             id="form3Example1m"
-                            class="form-control form-control-lg"
+                            className="form-control form-control-lg"
                             placeholder="Name"
+                            autoFocus
                           />
                         </div>
                       </div>
-                      <div class="col-md-6 mb-4">
-                        <div class="form-outline">
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
                           <input
                             ref={emailRef}
                             type="email"
                             id="form3Example1m"
-                            class="form-control form-control-lg"
+                            className="form-control form-control-lg"
                             placeholder="Email"
                           />
                         </div>
                       </div>
                     </div>
-                    <div class="row">
-                      <div class="col-md-6 mb-4">
-                        <div class="form-outline">
+                    <div className="row">
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
                           <input
                             ref={passwordRef}
                             type="password"
                             id="form3Example1m"
-                            class="form-control form-control-lg"
+                            className="form-control form-control-lg"
                             placeholder="Password"
                           />
                         </div>
                       </div>
-                      <div class="col-md-6 mb-4">
-                        <div class="form-outline">
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
                           <input
                             ref={confirmPwdRef}
                             type="password"
                             id="form3Example1m"
-                            class="form-control form-control-lg"
+                            className="form-control form-control-lg"
                             placeholder="Confirm Password"
                           />
                         </div>
                       </div>
                     </div>
 
-                    <div class="d-flex justify-content-end pt-3">
+                    <div className="d-flex justify-content-end pt-3">
                       <button
                         type="button"
-                        class="btn btn-warning btn-lg ms-2"
+                        className="btn btn-warning btn-lg ms-2"
                         onClick={register}
                       >
                         Register
